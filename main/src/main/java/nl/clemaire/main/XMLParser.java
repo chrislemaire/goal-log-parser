7e828ae0732eca678d684ba03f4694c21ed96be4
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-02 23:13:28 +0200

    Add initial XML Parser

---
+++ main/src/main/java/nl/clemaire/main/XMLParser.java
@@ -0,0 +1,28 @@
+package nl.clemaire.main;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Chris Lemaire on 2-6-2017.
+ */
+public class XMLParser {
+
+    public Document parse(String filePath) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(true);
+        factory.setIgnoringElementContentWhitespace(true);
+
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        File file = new File(filePath);
+        return builder.parse(file);
+    }
+
+}
