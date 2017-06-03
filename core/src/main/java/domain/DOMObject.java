192f96a514186cc3a30fe6612f258fd9d221a031
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-03 08:57:51 +0200

    Add DOMObject and main Record DOMObject

---
+++ core/src/main/java/domain/DOMObject.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+/**
+ * Created by Chris Lemaire on 3-6-2017.
+ */
+public abstract class DOMObject {
+
+    protected Node node;
+
+    protected Object value;
+
+    public DOMObject(Node node) {
+        this.node = node;
+    }
+
+    public abstract void parse();
+
+    public Object getValue() {
+        return value;
+    }
+}
---
+++ core/src/main/java/domain/Record.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.w3c.dom.Node;
+
+/**
+ * Created by Chris Lemaire on 3-6-2017.
+ */
+public class Record extends DOMObject {
+
+    public Record(Node node) {
+        super(node);
+
+        assert node != null;
+        assert "record".equals(node.getNodeName());
+    }
+
+    public void parse() {
+
+    }
+
+}
