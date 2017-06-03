ff460e48402e2c5d785a19dc03402898d16e31c8
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-03 10:20:34 +0200

    Let Record use enumeration to instantiate childs

--- core/src/main/java/nl/clemaire/domain/Record.java
+++ core/src/main/java/nl/clemaire/domain/Record.java
@@ -1,6 +1,10 @@
 package nl.clemaire.domain;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Lemaire on 3-6-2017.
@@ -10,12 +14,26 @@ public class Record extends DOMObject {
     public Record(Node node) {
         super(node);
 
-        assert node != null;
         assert "record".equals(node.getNodeName());
     }
 
     public void parse() {
+        NodeList childs = node.getChildNodes();
 
+        List<DOMObject> buffer = new ArrayList<DOMObject>();
+        for (int i = 0; i < childs.getLength(); i++) {
+            Node child = childs.item(i);
+
+            RecordChildType type = RecordChildType.classify(child);
+            if (type == null) {
+                System.err.println("Not a matching type available for node '" + node
+                        + "'\n\tin record: '" + node.getNodeValue());
+            } else {
+                buffer.add(type.instantiate(child));
+            }
+        }
+
+        value = buffer;
     }
 
 }
