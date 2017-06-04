d94ccf20da0d108c7c5b396e4f99e9f2f949b1de
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-05 01:08:02 +0200

    Add Main method/class

---
+++ core/src/main/java/nl/clemaire/Main.java
@@ -0,0 +1,29 @@
+package nl.clemaire;
+
+import nl.clemaire.crawler.LogViewer;
+
+import java.io.IOException;
+
+/**
+ * Created by Chris Lemaire on 4-6-2017.
+ */
+public class Main {
+
+    private static String PERFORMANCE_LOG_MODE = "PF_LOG_MODE";
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Not enough command line arguments: " + args.length
+                    + "\n\tNeed 2 to run: log-parse-mode and logfile-path.");
+        } else {
+            if (PERFORMANCE_LOG_MODE.equals(args[0])) {
+                try {
+                    LogViewer.showLogFileMatrix(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
