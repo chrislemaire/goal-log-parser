ae3e27814480f73a21a2b81c8b0290faa7c4f766
Chris Lemaire <c.lemaire@student.tudelft.nl>
2017-06-08 22:22:28 +0200

    Document DOMObject and Message

--- core/src/main/java/nl/clemaire/domain/DOMObject.java
+++ core/src/main/java/nl/clemaire/domain/DOMObject.java
@@ -3,14 +3,33 @@ package nl.clemaire.domain;
 import org.w3c.dom.Node;
 
 /**
- * Created by Chris Lemaire on 3-6-2017.
+ * Simple superclass for classes representing {@link Node}
+ * objects. Holds a reference to the original {@link Node}
+ * and an {@link Object} field that represents the value of
+ * the {@link Node}.
+ *
+ * @author Chris Lemaire
  */
 public abstract class DOMObject {
 
+    /**
+     * Reference to the original {@link Node}
+     * this {@link DOMObject} represents.
+     */
     protected Node node;
 
+    /**
+     * Object representing the (important) value(s)
+     * recorded in the DOM {@link Node}.
+     */
     protected Object value;
 
+    /**
+     * Creates a new {@link DOMObject} from the {@link Node}
+     * it represents.
+     *
+     * @param node this {@link DOMObject} should represent.
+     */
     public DOMObject(Node node) {
         assert node != null;
         this.node = node;
@@ -18,13 +37,29 @@ public abstract class DOMObject {
         parse();
     }
 
+    /**
+     * Parses the {@link Node} to get all interesting values
+     * from it and store it as a single {@link Object} in the
+     * {@link #value} field.
+     */
     public abstract void parse();
 
+    /**
+     * Gets the {@link #node} field.
+     *
+     * @return the {@link #node} field.
+     */
     public Node getNode() {
         return node;
     }
 
+    /**
+     * Gets the {@link #value} field.
+     *
+     * @return the {@link #value} field as an {@link Object}.
+     */
     public Object getValue() {
         return value;
     }
+
 }
--- core/src/main/java/nl/clemaire/domain/objects/message/Message.java
+++ core/src/main/java/nl/clemaire/domain/objects/message/Message.java
@@ -6,16 +6,29 @@ import org.w3c.dom.Node;
 import java.util.InputMismatchException;
 
 /**
- * Created by Chris Lemaire on 3-6-2017.
+ * {@link DOMObject} for the message {@link nl.clemaire.domain.NodeType}.
+ * Adds methods for getting and checking the presence of individual
+ * {@link MessageLine}s.
+ *
+ * @see MessageLine
+ * @see MessageType
+ *
+ * @author Chris Lemaire
  */
 public class Message extends DOMObject {
 
+    /**
+     * Creates a new {@link Message} from super constructor.
+     *
+     * @param node this {@link Message} object should represent.
+     */
     public Message(Node node) {
         super(node);
 
         assert "message".equals(node.getNodeName());
     }
 
+    @Override
     public void parse() {
         String[] lines = node.getTextContent().split("\n");
 
@@ -31,10 +44,25 @@ public class Message extends DOMObject {
         value = buffer;
     }
 
+    /**
+     * Gets all {@link MessageLine}s contained in this one
+     * {@link Message}.
+     *
+     * @return a list of all {@link MessageLine}s in this {@link Message}.
+     */
     public MessageLine[] getLines() {
         return (MessageLine[]) getValue();
     }
 
+    /**
+     * The first line of a certain {@link MessageType}. This assumes
+     * there is only a single {@link MessageLine} of the
+     * requested type.
+     *
+     * @param type the returned line must match.
+     * @return first {@link MessageLine} of given type or <code>null</code>
+     *          if none is found.
+     */
     public MessageLine getLine(MessageType type) {
         for (MessageLine line : getLines()) {
             if (line != null && line.getType() == type) {
@@ -44,6 +72,14 @@ public class Message extends DOMObject {
         return null;
     }
 
+    /**
+     * Checks whether there is a {@link MessageLine} of given
+     * {@link MessageType}.
+     *
+     * @param type the matched {@link MessageLine} must be.
+     * @return <code>true</code> when a {@link MessageLine} is
+     *          found of the given type.
+     */
     public boolean hasLine(MessageType type) {
         return getLine(type) != null;
     }
